3ec7d38e434587e0fffc2faefcdf16a34fafda96
haobingo <15879624465@sina.cn>
2024-07-19 13:13:20 +0800

    2024.7.19 继承已有课程信息功能

--- src/main/java/com/example/mapper/CourseBasicInformationMAPPER.java
+++ src/main/java/com/example/mapper/CourseBasicInformationMAPPER.java
@@ -2,9 +2,13 @@ package com.example.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.object.CourseBasicInformation;
+import com.example.object.CourseSyllabusInformation;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
 
 @Mapper
 public interface CourseBasicInformationMAPPER extends BaseMapper<CourseBasicInformation> {
@@ -13,4 +17,9 @@ public interface CourseBasicInformationMAPPER extends BaseMapper<CourseBasicInfo
             "set accomplish = 'true'\n" +
             "where id = #{courseId};")
     Boolean updateStatus(@Param("courseId") int courseId);
+
+
+    @Select("SELECT major, course_name FROM course_syllabus_information ORDER BY major, course_name")
+    List<CourseSyllabusInformation> getCourses();
+
 }
